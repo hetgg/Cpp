0138191af9996740c1f2486e53f2fd0f25f62e74
hetgg <54173786+hetgg@users.noreply.github.com>
2023-07-16 16:26:44 -0700

    temp

---
+++ const.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+int main(){
+
+    const int x = 7;
+    std::cout << x << std::endl;
+
+    int x = 10; // overwriting the value
+    std::cout << x << std::endl;
+
+    return 0;
+
+}
